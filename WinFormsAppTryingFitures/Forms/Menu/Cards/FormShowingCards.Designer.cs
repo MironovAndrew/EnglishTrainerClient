fbf734f81f6bc21257c9262bf19921a734379ef6
MironovAndrew <71024447+MironovAndrew@users.noreply.github.com>
2022-12-06 14:07:30 +0300

    Завершил карточки

--- WinFormsAppTryingFitures/Forms/Menu/Cards/FormShowingCards.Designer.cs
+++ WinFormsAppTryingFitures/Forms/Menu/Cards/FormShowingCards.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsAppTryingFitures.Forms.Menu.Cards
+﻿namespace WinFormsAppTryingFitures
 {
     partial class FormShowingCards
     {
@@ -30,77 +30,63 @@
         {
             this.panelMain = new System.Windows.Forms.Panel();
             this.labelScore = new System.Windows.Forms.Label();
-            this.buttonCheck = new System.Windows.Forms.Button();
             this.textBoxAnswer = new System.Windows.Forms.TextBox();
-            this.ButtonShowWordPhrase = new WinFormsAppTryingFitures.RoundButton();
+            this.ButtonShow = new WinFormsAppTryingFitures.RoundButton();
             this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
             // 
             this.panelMain.Controls.Add(this.labelScore);
-            this.panelMain.Controls.Add(this.buttonCheck);
             this.panelMain.Controls.Add(this.textBoxAnswer);
-            this.panelMain.Controls.Add(this.ButtonShowWordPhrase);
+            this.panelMain.Controls.Add(this.ButtonShow);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(0, 0);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(800, 450);
+            this.panelMain.Size = new System.Drawing.Size(800, 511);
             this.panelMain.TabIndex = 2;
             // 
             // labelScore
             // 
             this.labelScore.AutoSize = true;
             this.labelScore.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelScore.Location = new System.Drawing.Point(9, 11);
+            this.labelScore.Location = new System.Drawing.Point(380, 11);
             this.labelScore.Name = "labelScore";
             this.labelScore.Size = new System.Drawing.Size(40, 22);
             this.labelScore.TabIndex = 10;
-            this.labelScore.Text = "0/0";
-            // 
-            // buttonCheck
-            // 
-            this.buttonCheck.BackColor = System.Drawing.Color.YellowGreen;
-            this.buttonCheck.FlatAppearance.BorderSize = 0;
-            this.buttonCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCheck.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCheck.Location = new System.Drawing.Point(188, 429);
-            this.buttonCheck.Name = "buttonCheck";
-            this.buttonCheck.Size = new System.Drawing.Size(232, 28);
-            this.buttonCheck.TabIndex = 9;
-            this.buttonCheck.Text = "Проверить";
-            this.buttonCheck.UseVisualStyleBackColor = false;
+            this.labelScore.Text = "0/1";
             // 
             // textBoxAnswer
             // 
             this.textBoxAnswer.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBoxAnswer.Location = new System.Drawing.Point(188, 392);
+            this.textBoxAnswer.Location = new System.Drawing.Point(284, 414);
             this.textBoxAnswer.Name = "textBoxAnswer";
             this.textBoxAnswer.Size = new System.Drawing.Size(232, 31);
             this.textBoxAnswer.TabIndex = 8;
             // 
-            // ButtonShowWordPhrase
+            // ButtonShow
             // 
-            this.ButtonShowWordPhrase.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ButtonShowWordPhrase.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonShowWordPhrase.Enabled = false;
-            this.ButtonShowWordPhrase.FlatAppearance.BorderSize = 0;
-            this.ButtonShowWordPhrase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ButtonShowWordPhrase.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonShowWordPhrase.Location = new System.Drawing.Point(154, 86);
-            this.ButtonShowWordPhrase.Name = "ButtonShowWordPhrase";
-            this.ButtonShowWordPhrase.Size = new System.Drawing.Size(300, 300);
-            this.ButtonShowWordPhrase.TabIndex = 6;
-            this.ButtonShowWordPhrase.Text = "Computer";
-            this.ButtonShowWordPhrase.UseVisualStyleBackColor = false;
+            this.ButtonShow.BackColor = System.Drawing.Color.YellowGreen;
+            this.ButtonShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonShow.FlatAppearance.BorderSize = 0;
+            this.ButtonShow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonShow.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ButtonShow.Location = new System.Drawing.Point(250, 86);
+            this.ButtonShow.Name = "ButtonShow";
+            this.ButtonShow.Size = new System.Drawing.Size(300, 300);
+            this.ButtonShow.TabIndex = 6;
+            this.ButtonShow.Text = "Начать!";
+            this.ButtonShow.UseVisualStyleBackColor = false;
             // 
             // FormShowingCards
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 511);
             this.Controls.Add(this.panelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormShowingCards";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormShowingCards";
             this.panelMain.ResumeLayout(false);
             this.panelMain.PerformLayout();
@@ -112,8 +98,7 @@
 
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Label labelScore;
-        private System.Windows.Forms.Button buttonCheck;
         private System.Windows.Forms.TextBox textBoxAnswer;
-        private RoundButton ButtonShowWordPhrase;
+        private RoundButton ButtonShow;
     }
 }
--- WinFormsAppTryingFitures/Forms/Menu/Cards/FormShowingCards.cs
+++ WinFormsAppTryingFitures/Forms/Menu/Cards/FormShowingCards.cs
@@ -8,13 +8,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinFormsAppTryingFitures.Forms.Menu.Cards
+namespace WinFormsAppTryingFitures
 {
     public partial class FormShowingCards : Form
     {
-        public FormShowingCards()
+        public FormShowingCards(Dictionary<string, string> dict)
         {
             InitializeComponent();
+
+            int questionNumber = 0;
+            int rightAnswers = 0;
+
+
+            ButtonShow.Text = getQuestionAnswer(dict, questionNumber).english;
+
+
+            ButtonShow.Click += (a, b) =>
+            {
+
+
+                if (textBoxAnswer.Text == getQuestionAnswer(dict, questionNumber).russian)
+                {
+                    rightAnswers++;
+                }
+                else
+                {
+                    MessageBox.Show($"Неверно!\n{getQuestionAnswer(dict, questionNumber).english} = {getQuestionAnswer(dict, questionNumber).russian}");
+                }
+
+                ++questionNumber;
+                labelScore.Text = $"{rightAnswers}/{questionNumber +1}";
+
+
+                ButtonShow.Text = getQuestionAnswer(dict, questionNumber).english;
+            };
+        }
+
+
+        (string english, string russian) getQuestionAnswer(Dictionary<string, string> dict, int index)
+        {
+            string english = "";
+            string russian = "";
+
+
+            if (index < dict.Count)
+            {
+                english = dict.Keys.ElementAt(index);
+                russian = dict.Values.ElementAt(index);
+            }
+            else
+            {
+                MessageBox.Show("Game over");
+                new FormSettingCards().Show();
+                this.Hide();
+            }
+
+
+            return (english, russian);
+
         }
     }
 }
